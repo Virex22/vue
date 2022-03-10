2faab229fcb4fa49ec793c3f30483dac7920c9ac
Vincent REMY <vincent@why.vision>
2022-03-10 09:39:32 +0100

    kdo

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <todo title="c'est ma props"></todo>
+</template>
+<script type="module">
+import todo from "./compenents/todo.vue";
+
+export default {
+    components: { todo }
+}
+</script>
---
+++ src/compenents/addInput.vue
@@ -0,0 +1,32 @@
+<template>      
+<div class="flex mt-4">
+                  <input
+                      v-model="description"
+                      class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-700"
+                      placeholder="Add Todo"
+                      @keyup.enter="addTodo()"
+                  >
+                  <button
+                      @click="addTodo()"
+                      class="flex p-2 border-2 rounded text-blue-500 border-blue-500 hover:text-white hover:bg-blue-500"
+                  >Ajouter</button>
+              </div>   
+</template>
+
+<script>
+export default {
+    name: "list-element",
+    data(){return {
+        description:"",
+    }},
+    props : {
+        onSaveTodo :Function,
+    },
+    methods: {
+        addTodo(){
+            this.$emit("onSaveTodo",this.description);
+            this.description = "";
+        }
+    },
+}
+</script>
